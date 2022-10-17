394933927a68eddfaec9f4b936580d800ad6c81a
sonya-bulanova <60941559+sonya-bulanova@users.noreply.github.com>
2022-10-17 13:51:43 +0300

    Lab #3

--- task1/main.py
+++ task1/main.py
@@ -1,1 +1,11 @@
-print("Hello World")
+src = not False and True or False and not True
+
+# TODO расписать упрощение выражения
+
+# result = (True and True) or (False and False)
+# result = True or False
+# result = True
+
+result = True  # TODO подставить результат выражения
+
+print(src == result)
